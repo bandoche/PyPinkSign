f4932b31a952c1a0a58aa5e34e7b2249fed23d48
Sang-jun Jung <bandoche@hanmail.net>
2019-03-22 11:32:23 +0900

    Add python3 feature

--- setup.py
+++ setup.py
@@ -46,6 +46,8 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7'
     ],
     **EXTRAS
 )
