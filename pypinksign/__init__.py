491613b34cb3c89e8d49670457a46b924a109529
Sangjun Jung <bandoche@hanmail.net>
2019-05-12 08:57:07 +0900

    Add new function in init

--- pypinksign/__init__.py
+++ pypinksign/__init__.py
@@ -6,5 +6,5 @@ similar to the template part of PasteScript but without any dependencies.
 
 from .pypinksign import (
     PinkSign, get_npki_path, url_encode, paramize, choose_cert, seed_cbc_128_encrypt, seed_cbc_128_decrypt, seed_generator,
-    bit2string, separate_p12_into_npki
+    bit2string, separate_p12_into_npki, encrypt_decrypted_prikey
 )
