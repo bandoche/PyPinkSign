11c28592626faeb99d041527274690ad4b042708
Sang-jun Jung <bandoche@hanmail.net>
2019-05-22 14:04:29 +0900

    Add function in init and ignore jet brain related folder

--- pypinksign/__init__.py
+++ pypinksign/__init__.py
@@ -6,5 +6,5 @@ similar to the template part of PasteScript but without any dependencies.
 
 from .pypinksign import (
     PinkSign, get_npki_path, url_encode, paramize, choose_cert, seed_cbc_128_encrypt, seed_cbc_128_decrypt, seed_generator,
-    bit2string, separate_p12_into_npki, encrypt_decrypted_prikey
+    bit2string, separate_p12_into_npki, encrypt_decrypted_prikey, inject_rand_in_plain_prikey
 )
