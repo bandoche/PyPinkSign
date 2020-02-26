40ce95c2c7c377faacaecbf3281c2bac4cd377a7
Sang-jun Jung <bandoche@hanmail.net>
2020-02-26 09:57:31 +0900

    add version in init and update comments and lint

--- pypinksign/__init__.py
+++ pypinksign/__init__.py
@@ -1,10 +1,11 @@
 """
-Basic Template system for project pinksign,
-similar to the template part of PasteScript but without any dependencies.
-
+PyPinkSign - A simple package for managing certifications in Korean (NPKI)
 """
 
 from .pypinksign import (
-    PinkSign, get_npki_path, url_encode, paramize, choose_cert, seed_cbc_128_encrypt, seed_cbc_128_decrypt, seed_generator,
-    bit2string, separate_p12_into_npki, encrypt_decrypted_prikey, inject_rand_in_plain_prikey
+    PinkSign, get_npki_path, url_encode, paramize, choose_cert, seed_cbc_128_encrypt, seed_cbc_128_decrypt,
+    seed_generator, bit2string, separate_p12_into_npki, encrypt_decrypted_prikey, inject_rand_in_plain_prikey
 )
+
+# https://www.python.org/dev/peps/pep-0396/
+__version__ = '0.3.0'
