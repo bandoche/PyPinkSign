fb300c63cf2d34244df433c697ae70db20f77091
Sang-jun Jung <bandoche@hanmail.net>
2020-02-26 12:00:53 +0900

    removed function

--- pypinksign/__init__.py
+++ pypinksign/__init__.py
@@ -3,8 +3,8 @@ PyPinkSign - A simple package for managing certifications in Korean (NPKI)
 """
 
 from .pypinksign import (
-    PinkSign, get_npki_path, url_encode, paramize, choose_cert, seed_cbc_128_encrypt, seed_cbc_128_decrypt,
-    seed_generator, bit2string, separate_p12_into_npki, encrypt_decrypted_prikey, inject_rand_in_plain_prikey
+    PinkSign, get_npki_path, choose_cert, seed_cbc_128_encrypt, seed_cbc_128_decrypt,
+    seed_generator, separate_p12_into_npki, encrypt_decrypted_prikey, inject_rand_in_plain_prikey
 )
 
 # https://www.python.org/dev/peps/pep-0396/
